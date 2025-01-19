fb0bca9827783945e9f56d5d3cd2fdf492c93b99
Sameer Irfan <sameerirfangaba@gmail.com>
2025-01-19 05:11:31 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 app = Flask(__name__)
 
-openai.api_key = os.getenv(OPENAI_API_KEY)
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
